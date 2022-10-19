61266b1685d6b699012440daa4b76c54c2816f05
Saiket Das <saiketdas02@gmail.com>
2022-10-19 23:07:29 +0600

    Backend - Assign a staff controller

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -20,3 +20,24 @@ exports.assignAdmin = async (req, res) => {
     });
   }
 };
+
+// ---------> MAKE STAFF
+exports.assignStaff = async (req, res) => {
+  try {
+    const reuslt = await assignStaffService(req.params.id);
+
+    if (reuslt.modifiedCount > 0) {
+      res.status(200).json({
+        status: "Success",
+        message: "Successfully assign an hiring manager",
+        data: reuslt,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: "Fail",
+      message: " Internal server error",
+      error: error.message,
+    });
+  }
+};
