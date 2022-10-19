5b2a6cc6228d5c24e7ff3693d10fc9047f9de2b9
Saiket Das <saiketdas02@gmail.com>
2022-10-19 23:03:33 +0600

    Backend - Assign an admin service

--- backend/services/admin.service.js
+++ backend/services/admin.service.js
@@ -1,1 +1,10 @@
+const User = require("../models/user.model");
 
+exports.assignAdminService = async (id) => {
+  const result = await User.updateOne(
+    { _id: id },
+    { $set: { role: "admin" } },
+    { runValidators: true }
+  );
+  return result;
+};
