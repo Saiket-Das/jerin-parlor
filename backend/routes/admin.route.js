9c0c65228fac3084a32c00ea20eade73d407cb78
Saiket Das <saiketdas02@gmail.com>
2022-10-19 23:06:51 +0600

    Backend - Assign a staff route

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/user.controller");
+const adminController = require("../controllers/admin.controller");
 const verifyToken = require("../middlewares/verifyToken");
 const authorization = require("../middlewares/authorization");
 
-router.route("/assignAdmin/:id").patch(verifyToken, userController.assignAdmin);
+router
+  .route("/assignAdmin/:id")
+  .patch(verifyToken, adminController.assignAdmin);
 
-// router
-//   .route("/assignManager/:id")
-//   .patch(verifyToken, authorization("admin"), userController.assignManager);
-
-// router.get("/me", verifyToken, userController.getMe);
+router
+  .route("/assignManager/:id")
+  .patch(verifyToken, adminController.assignStaff);
 
 module.exports = router;
