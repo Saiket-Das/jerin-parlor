0305b10727161627e210f991d07f6977e316d95e
Saiket Das <saiketdas02@gmail.com>
2022-11-07 21:32:36 +0600

    Frontend - Header component created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Card from "./app/components/common/Card";
+import Header from "./app/components/common/Header";
 
 const App = () => {
   return (
     <div>
-      <Card />
+      <Header />
     </div>
   );
 };
