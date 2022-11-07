07bc12aa4216dc8a968304dd20fa1987852f3517
Saiket Das <saiketdas02@gmail.com>
2022-11-07 19:45:14 +0600

    Frontend - Tailwind CSS installed

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,31 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./app/**/*.{js,ts,jsx,tsx}",
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     [
+//       "postcss-preset-env",
+//       {
+//         // Options
+//       },
+//     ],
+//   ],
+// };
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
