bffbfec1d4e8f3911f96a9c7d6b2c9b523120a4c
Saiket Das <saiketdas02@gmail.com>
2022-11-07 22:19:33 +0600

    Frontend - App Button component created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,15 @@ import Card from "./app/components/shared/Card";
 import Footer from "./app/components/shared/Footer";
 import Header from "./app/components/shared/Header";
 import Loading from "./app/components/shared/Loading";
+import AppButton from "./app/components/shared/AppButton";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <Loading />
+      {/* <Loading /> */}
+
+      <AppButton>Explore more</AppButton>
 
       {/* <Footer /> */}
     </div>
---
+++ frontend/src/app/components/shared/AppButton.tsx
@@ -0,0 +1,16 @@
+type Props = {
+  children: string;
+};
+
+const AppButton = ({ children }: Props) => {
+  return (
+    <button
+      type="button"
+      className="inline-block px-9 py-3 bg-primary text-white font-medium leading-snug rounded shadow-sm hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg transition duration-150 ease-in-out"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default AppButton;
