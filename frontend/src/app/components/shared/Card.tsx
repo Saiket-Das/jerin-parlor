0c0beb4ed8de30ffc505f3ebe52ee24787d70961
Saiket Das <saiketdas02@gmail.com>
2022-11-08 18:57:52 +0600

    Frontend - Card component's styles updated

--- frontend/src/app/components/shared/Card.tsx
+++ frontend/src/app/components/shared/Card.tsx
@@ -3,14 +3,20 @@ import faceCare from "../../assets/images/face-care.png";
 const Card = () => {
   return (
     <div className="card w-[370px] bg-base-100 hover:shadow-xl px-5">
-      <div className="py-7 flex justify-center ">
-        <figure className="bg-base-200 h-16 w-16 rounded-full">
+      <div className="pt-7 flex justify-center ">
+        <figure className="bg-primary h-16 w-16 rounded-full">
           <img src={faceCare} alt="Shoes" className="rounded-xl w-12 h-12" />
         </figure>
       </div>
-      <div className=" m-0">
-        <h2 className="card-title">Hair Color & Styleing</h2>
-        <p>If a dog chews shoes /..whose shoes does he choose?</p>
+      <div className="flex justify-center px-6 pb-6">
+        <div className="text-center m-0">
+          <h2 className="font-semibold text-xl pt-5">Hair Color & Styleing</h2>
+          <p className="font-medium text-xl text-primary p-2">$99</p>
+          <p className="font-light text-gray-400">
+            Amazing flyers, social media posts and brand representations that
+            would make your brand stand out.
+          </p>
+        </div>
       </div>
     </div>
   );
