c29fe7f81f9ad056f9c4d3b92e13bcd20d089cb8
Saiket Das <saiketdas02@gmail.com>
2022-11-09 17:52:27 +0600

    Frontent - Dashboard sidebar created

--- frontend/src/app/pages/dashboard/Dashboard.tsx
+++ frontend/src/app/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div>
-      <p>Dashboard</p>
+      <div className="drawer drawer-mobile">
+        <input
+          id="dashboard-sidebar"
+          type="checkbox"
+          className="drawer-toggle"
+        />
+        <div className="drawer-content">
+          <h2 className="text-3xl text-purple-500 font-bold text-center">
+            My Dashboard
+          </h2>
+          <Outlet></Outlet>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
+          <ul className="menu p-4 overflow-y-auto w-56 bg-base-100 text-base-content">
+            {/* <!-- Sidebar content here --> */}
+            <li>
+              <Link to="/dashboard">Book</Link>
+            </li>
+            <li>
+              <Link to="/dashboard/review">Booking List</Link>
+            </li>
+            <li>
+              <Link to="/dashboard/history">Review</Link>
+            </li>
+            {/* {
+                        admin && <>
+                            <li><Link to='/dashboard/users'> All Users</Link></li>
+                            <li><Link to='/dashboard/doctors'> Add a doctors</Link></li>
+                            <li><Link to='/dashboard/manageDoctors'> Manage Doctors</Link></li>
+                        </>
+                    } */}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
