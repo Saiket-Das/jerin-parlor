5d9e3630baa3c26050c9bea4274432dba126b8a3
Saiket Das <saiketdas02@gmail.com>
2022-11-11 18:34:44 +0600

    Frontend - Table added in Order List

--- frontend/src/app/pages/dashboard/admin/OrderList.tsx
+++ frontend/src/app/pages/dashboard/admin/OrderList.tsx
@@ -1,7 +1,29 @@
 const OrderList = () => {
   return (
-    <div>
-      <p>Order List</p>
+    <div className="bg-white p-4">
+      <div className="overflow-x-auto ">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th className="bg-base-300">No</th>
+              <th className="bg-base-300">Name</th>
+              <th className="bg-base-300">Email</th>
+              <th className="bg-base-300">Service</th>
+              <th className="bg-base-300">Pay with</th>
+              <th className="bg-base-300">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Ahan Bryan</td>
+              <td>ahanbryan@gmail.com</td>
+              <td>Hair Treatment</td>
+              <td>Credit card</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
