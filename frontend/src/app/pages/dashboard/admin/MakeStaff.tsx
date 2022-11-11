6b8b01c7c42eaaa16612c4ec90d064b7e2e7e018
Saiket Das <saiketdas02@gmail.com>
2022-11-11 19:36:54 +0600

    Frontend - MakeStaff page's styles updated

--- frontend/src/app/pages/dashboard/admin/MakeStaff.tsx
+++ frontend/src/app/pages/dashboard/admin/MakeStaff.tsx
@@ -1,7 +1,18 @@
+import AppButton from "../../../components/shared/AppButton";
+import Input from "../../../components/shared/AppInput";
+
 const MakeStaff = () => {
   return (
-    <div>
-      <p>Make Staff</p>
+    <div className="bg-white p-4">
+      <div className="flex gap-10 items-center ">
+        <Input
+          label="Email"
+          type="text"
+          name="email"
+          placeholder="Search by email..."
+        />
+        <AppButton>Submit</AppButton>
+      </div>
     </div>
   );
 };
