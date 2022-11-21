9b13445a6f33a60776ca645478efdc22b410d8f5
Saiket Das <saiketdas02@gmail.com>
2022-11-21 20:17:50 +0600

    Frontend - Review component added in Testimonial component

--- frontend/src/app/components/home/Testimonial.tsx
+++ frontend/src/app/components/home/Testimonial.tsx
@@ -4,6 +4,12 @@ const Testimonial = () => {
   return (
     <div className="mt-32">
       <h2 className="text-[34px] font-semibold text-center">Testimonial</h2>
+
+      <div className="mt-[60px] grid grid-cols-3 gap-10">
+        <Review />
+        <Review />
+        <Review />
+      </div>
     </div>
   );
 };
