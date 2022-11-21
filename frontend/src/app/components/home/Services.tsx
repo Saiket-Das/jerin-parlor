c7540fd007e3618abd27b499349597b92c7031ea
Saiket Das <saiketdas02@gmail.com>
2022-11-21 20:08:09 +0600

    Frontend - Card component added in Service component

--- frontend/src/app/components/home/Services.tsx
+++ frontend/src/app/components/home/Services.tsx
@@ -1,3 +1,5 @@
+import Card from "../shared/Card";
+
 const Services = () => {
   return (
     <div className="mt-32">
@@ -5,6 +7,12 @@ const Services = () => {
         <h2 className="text-4xl font-semibold text-center">
           Our Awesome <span className="text-primary">Services</span>
         </h2>
+
+        <div className="mt-[72px] grid grid-cols-3  gap-10">
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </div>
     </div>
   );
