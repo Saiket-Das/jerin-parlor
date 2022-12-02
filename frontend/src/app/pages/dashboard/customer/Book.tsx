bbc646baaf423681638d5d4cc62298a69eff6417
Saiket Das <saiketdas02@gmail.com>
2022-12-02 21:52:39 +0600

    Frontend - Styles updated in Book component

--- frontend/src/app/pages/dashboard/customer/Book.tsx
+++ frontend/src/app/pages/dashboard/customer/Book.tsx
@@ -4,18 +4,31 @@ import Input from "../../../components/shared/AppInput";
 const Book = () => {
   return (
     <div>
-      <Input type="text" name="name" placeholder="Ahan Bryan" readOnly={true} />
+      <div className="mb-5">
+        <Input
+          type="text"
+          name="name"
+          placeholder="Ahan Bryan"
+          readOnly={true}
+        />
+      </div>
 
-      <Input
-        type="text"
-        name="email"
-        placeholder="ahanbryan@gmail.com"
-        readOnly={true}
-      />
+      <div className="mb-5">
+        <Input
+          type="text"
+          name="email"
+          placeholder="ahanbryan@gmail.com"
+          readOnly={true}
+        />
+      </div>
 
-      <Input type="text" name="Tname" placeholder="Treatment name" />
+      <div className="mb-5">
+        <Input type="text" name="Tname" placeholder="Treatment name" />
+      </div>
 
-      <Input type="number" name="price" placeholder="Treatment price" />
+      <div className="mb-5">
+        <Input type="number" name="price" placeholder="Treatment price" />
+      </div>
 
       <Button>Checkout</Button>
     </div>
