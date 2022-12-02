54219ae076bae44b82617b140528b89234f0456a
Saiket Das <saiketdas02@gmail.com>
2022-12-02 21:35:07 +0600

    Frontend - Service data implemented in Service component

--- frontend/src/app/components/home/Services.tsx
+++ frontend/src/app/components/home/Services.tsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
 import Card from "../shared/Card";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
+
   return (
     <div className="mt-32 mb-24 mx-6 md:mx-20 lg:mx-36">
       <h2 className="text-[34px] font-semibold text-center">
@@ -8,9 +17,9 @@ const Services = () => {
       </h2>
 
       <div className="mt-[72px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-        <Card />
-        <Card />
-        <Card />
+        {services.map((service, index) => (
+          <Card key={index} service={service} />
+        ))}
       </div>
     </div>
   );
--- frontend/src/app/components/shared/Card.tsx
+++ frontend/src/app/components/shared/Card.tsx
@@ -1,6 +1,18 @@
 import faceCare from "../../assets/images/face-care.png";
 
-const Card = () => {
+type Props = {
+  service: {
+    _id: string;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+  };
+};
+
+const Card = ({
+  service: { _id, name, description, price, imageUrl },
+}: Props) => {
   return (
     <div className="card w-[370px] bg-base-100 hover:shadow-xl px-5">
       <div className="pt-7 flex justify-center ">
@@ -10,12 +22,9 @@ const Card = () => {
       </div>
       <div className="flex justify-center px-6 pb-6">
         <div className="text-center m-0">
-          <h2 className="font-semibold text-xl pt-5">Hair Color & Styleing</h2>
-          <p className="font-medium text-xl text-primary p-2">$99</p>
-          <p className="font-light text-gray-400">
-            Amazing flyers, social media posts and brand representations that
-            would make your brand stand out.
-          </p>
+          <h2 className="font-semibold text-xl pt-5">{name}</h2>
+          <p className="font-medium text-xl text-primary p-2">{price}</p>
+          <p className="font-light text-gray-400">{description}</p>
         </div>
       </div>
     </div>
