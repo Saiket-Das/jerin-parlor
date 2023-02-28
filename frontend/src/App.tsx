20241220da6ef2f4474efb467fc3893f8de942b1
Saiket Das <saiketdas02@gmail.com>
2023-02-28 23:59:44 +0600

    Frontend - Preloader added in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+import Loading from "./app/components/shared/Loading";
 import Layout from "./app/layout/Layout";
 import ReactRoutes from "./app/routes/ReactRoutes";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    <Loading />;
+    setLoading(false);
+  }, 2000);
   return (
     <div className="app">
-      <Layout>
-        <ReactRoutes />
-      </Layout>
+      {!loading && (
+        <Layout>
+          <ReactRoutes />
+        </Layout>
+      )}
     </div>
   );
 };
