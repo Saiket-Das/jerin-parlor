81db0601604ccfd7804a0167020f6565266a7a14
Saiket Das <saiketdas02@gmail.com>
2023-02-28 23:38:40 +0600

    Frontend - Comments added in pages/index.js file

--- frontend/src/app/pages/dashboard/Dashboard.tsx
+++ frontend/src/app/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import {
   faCartShopping,
-  faHardDrive,
   faMessage,
   faListCheck,
   faPlus,
--- frontend/src/app/pages/index.tsx
+++ frontend/src/app/pages/index.tsx
@@ -1,15 +1,20 @@
+// Home
 export { default as Home } from "./home/Home";
 
+// Login & Register
 export { default as Login } from "./auth/Login";
 export { default as Register } from "./auth/Register";
 
+// Dashboard
 export { default as Dashboard } from "./dashboard/Dashboard";
 
+// Dashboard - Admin
 export { default as AddService } from "./dashboard/admin/AddService";
 export { default as MakeStaff } from "./dashboard/admin/MakeStaff";
 export { default as ManageService } from "./dashboard/admin/ManageService";
 export { default as OrderList } from "./dashboard/admin/OrderList";
 
+// Dashboard - Customer
 export { default as Book } from "./dashboard/customer/Book";
 export { default as BookingList } from "./dashboard/customer/BookingList";
 export { default as Review } from "./dashboard/customer/Review";
