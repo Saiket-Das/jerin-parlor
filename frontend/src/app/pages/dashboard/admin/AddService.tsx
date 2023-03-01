ffd3818fe572dea89d4708b164406adde83fdda2
Saiket Das <saiketdas02@gmail.com>
2023-03-01 23:11:33 +0600

    Frontend - Price & Available time input fields added in AddService page

--- frontend/src/app/pages/dashboard/admin/AddService.tsx
+++ frontend/src/app/pages/dashboard/admin/AddService.tsx
@@ -3,17 +3,39 @@ import { Button, Input, Textarea } from "../../../components/shared/index";
 const AddService = () => {
   return (
     <div>
-      <div className="mb-5">
-        <Input
-          type="text"
-          name="name"
-          placeholder="Enter title"
-          label="Service title"
-        />
+      <div className="md:flex lg:flex md:gap-20 lg:flex-20">
+        <div className="mb-5">
+          <Input
+            type="text"
+            name="title"
+            placeholder="Enter title"
+            label="Service title"
+          />
+        </div>
+
+        <div className="mb-5">
+          <Input
+            type="number"
+            name="price"
+            placeholder="Enter price"
+            label="Service price"
+          />
+        </div>
       </div>
 
-      <div className="mb-5">
-        <Input type="file" name="price" placeholder="" label="Image" />
+      <div className="md:flex lg:flex md:gap-20 lg:flex-20">
+        <div className="mb-5">
+          <Input type="file" name="price" placeholder="" label="Image" />
+        </div>
+
+        <div className="mb-5">
+          <Input
+            type="text"
+            name="time"
+            placeholder="Choose available time"
+            label="Service time"
+          />
+        </div>
       </div>
 
       <div className="mb-5">
