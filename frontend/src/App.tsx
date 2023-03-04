e00b7f78ace3bc04f98a731f19ba5452a15786a8
Saiket Das <saiketdas02@gmail.com>
2023-03-04 23:13:28 +0600

    Nothing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import "./App.css";
+
 import { useEffect, useState } from "react";
 
 import Layout from "./app/layout/Layout";
--- frontend/src/app/components/shared/AppButton.tsx
+++ frontend/src/app/components/shared/AppButton.tsx
@@ -22,6 +22,8 @@ const AppButton = ({
           <button
             type="button"
             className={`inline-block w-[${width}] px-9 py-3 bg-primary text-white font-medium leading-snug rounded shadow-sm hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none active:shadow-lg transition duration-150 ease-in-out`}
+
+            // className="bttn bg-primary"
           >
             {children}
           </button>
