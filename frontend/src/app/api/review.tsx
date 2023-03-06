926882bdc71e49ae78bf130f828db386a5802a07
Saiket Das <saiketdas02@gmail.com>
2023-03-06 22:45:36 +0600

    Frontend - Review api created

---
+++ frontend/src/app/api/review.tsx
@@ -0,0 +1,19 @@
+import client from "./client";
+
+const getAllReview = async () => {
+  return await client.get("/api/review");
+};
+
+const postNewReview = async (data: any) => {
+  await client.post("/api/review", data);
+};
+
+const deleteReview = async (id: string) => {
+  return await client.delete(`/api/review/${id}`);
+};
+
+export default {
+  getAllReview,
+  postNewReview,
+  deleteReview,
+};
